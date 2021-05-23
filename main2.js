3948417bb69f86c5374cbf7e21846deff39a552d
Erzhan Bisimbeew <bisimbeew@icloud.com>
2021-05-23 16:42:09 +0300

    burger

--- main2.js
+++ main2.js
@@ -1,9 +1,16 @@
 const burgerBtn = document.querySelector(".hamburger");
 const overlay = document.querySelector( ".overlay");
 const wrapper = document.querySelector (".wrapper");
+const closeBtn = document.querySelector (".closeBtn");
+
 
 burgerBtn.addEventListener('click', e=>{
 	wrapper.style.display = 'none';
 	overlay.style.display = "flex";
 
+});
+
+closeBtn.addEventListener('click', e=>{
+	overlay.style.display = 'none';
+	wrapper.style. display = 'block';
 });
